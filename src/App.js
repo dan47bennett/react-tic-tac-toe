cbe9f39d691d23435ea74c9aced8acc475ffdb14
Dan Bennett <dghbennett47@gmail.com>
2020-06-27 22:25:19 +0100

    start using context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import { React, createContext } from 'react';
 import Game from './components/Game'
 
-const App = () => <Game/>
+export const AuthContext = createContext();   //context to check if the user is logged in
 
+const App = () => {
+    return(
+        <AuthContext.Provider>
+            <Game/>
+        </AuthContext.Provider>
+    )
+}
 
 export default App;
