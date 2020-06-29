5a1c8d631d5b925c07652312becfaf61b221ad9f
Dan Bennett <dghbennett47@gmail.com>
2020-06-29 17:51:53 +0100

    add private routing to stop logged out users

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Route, Redirect } from "react-router-dom";
-import { AuthContext } from "./Authorisation";
+import { AuthContext } from "./Auth";
 
 const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
   const {currentUser} = useContext(AuthContext);
