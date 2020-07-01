6901f74ffb5c6b5f4461bc11ba46491d106c10f5
Dan Bennett <dghbennett47@gmail.com>
2020-07-01 11:31:10 +0100

    add testing to the project

---
+++ src/test/helpers.test.js
@@ -0,0 +1,29 @@
+import { calculateWinner, isBoardFull } from '../helpers'
+
+
+test('Should accurately assess which player has won', () => {
+    const square = ['X', 'O', null, 'O', 'X', null, null, 'O', 'X']
+    const result = calculateWinner(square);
+    expect(result).toBe('X');
+});
+
+
+test('Should ascertain that neither player has won', () => {
+    const square = ['X', 'O', null, 'X', 'O', null, 'O', null, 'X']
+    const result = calculateWinner(square);
+    expect(result).toBe(null);
+});
+
+
+test('Should detect that the board is not full', () => {
+    const square = ['X', 'O', null, 'X', 'O', null, 'O', null, 'X']
+    const result = isBoardFull(square);
+    expect(result).toBe(false);
+});
+
+
+test('Should detect that the board is full', () => {
+    const square = ['X', 'O', 'X', 'X', 'O', 'X', 'O', 'O', 'X']
+    const result = isBoardFull(square);
+    expect(result).toBe(true);
+});
