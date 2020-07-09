16a7331be8d5fbd42b8b8348c13a444493bbe99f
Dan Bennett <dghbennett47@gmail.com>
2020-07-09 16:33:04 +0100

    feat: make login prettier

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,39 @@
-import React, { useCallback, useContext } from 'react'
-import firebase from 'firebase'
+import React, { useCallback, useContext } from "react";
+import firebase from "firebase";
 import { withRouter, Redirect } from "react-router";
-import { loginButtonStyles } from '../styles'
-import { AuthContext } from '../Auth';
-
+import { loginButtonStyles, loginDivStyle } from "../styles";
+import { AuthContext } from "../Auth";
 
 const Login = ({ history }) => {
-    const handleLogin = useCallback(
-        async function googleLogin() {
-            try {
-                const provider = await new firebase.auth.GoogleAuthProvider();
-                await firebase.auth().signInWithPopup(provider);
-                history.push('/')
-            } catch (error) {
-                alert(error);
-            }
-    }, [history]
-    );
+  const handleLogin = useCallback(
+    async function googleLogin() {
+      try {
+        const provider = await new firebase.auth.GoogleAuthProvider();
+        await firebase.auth().signInWithPopup(provider);
+        history.push("/");
+      } catch (error) {
+        alert(error);
+      }
+    },
+    [history]
+  );
 
-    const { currentUser } = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
 
-    if (currentUser) {
-        return <Redirect to='/'/>;
-    }
+  if (currentUser) {
+    return <Redirect to="/" />;
+  }
 
-
-    return (
-        <>
-            <div>Please log in with google</div>
-            <button style={loginButtonStyles} onClick ={handleLogin}/>
-        </>
-    )
-}
+  return (
+    <>
+      <div style={loginDivStyle}>
+        <h1>Please log in with google</h1>
+        <div style={loginDivStyle}>
+          <button style={loginButtonStyles} onClick={handleLogin} />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default withRouter(Login);
--- src/styles.js
+++ src/styles.js
@@ -1,71 +1,76 @@
 export const gameStyles = {
-    width: 'fit-content',
-    margin: '20px auto'
+  width: "fit-content",
+  margin: "20px auto",
 };
 
 export const displayMessageStyles = {
-    textAlign: 'center',
+  textAlign: "center",
 };
 
 const baseButtonStyles = {
-    backgroundColor: 'white',
-    borderStyle: 'none',
-    margin: '0 5px 10px 5px',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'contain',
-    width: '40px',
-    height: '40px'
+  backgroundColor: "white",
+  borderStyle: "none",
+  margin: "0 5px 10px 5px",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "contain",
+  width: "40px",
+  height: "40px",
 };
 
 export const buttonStyles = {
-    backButton: {
-        ...baseButtonStyles,
-        backgroundImage: `url(${require('./assets/previous.svg')})`,
-    },
-    resetButton: {
-        ...baseButtonStyles,
-        backgroundImage: `url(${require('./assets/reset.svg')})`,
-    },
-    nextButton: {
-        ...baseButtonStyles,
-        backgroundImage: `url(${require('./assets/next.svg')})`,
-    }
-
-}
+  backButton: {
+    ...baseButtonStyles,
+    backgroundImage: `url(${require("./assets/previous.svg")})`,
+  },
+  resetButton: {
+    ...baseButtonStyles,
+    backgroundImage: `url(${require("./assets/reset.svg")})`,
+  },
+  nextButton: {
+    ...baseButtonStyles,
+    backgroundImage: `url(${require("./assets/next.svg")})`,
+  },
+};
 
 export const boardStyles = {
-    border: '5px solid darkblue',
-    borderRadius: '10px',
-    width: '500px',
-    height: '500px',
-    margin: '0 auto',
-    display: 'grid',
-    gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
+  border: "5px solid darkblue",
+  borderRadius: "10px",
+  width: "500px",
+  height: "500px",
+  margin: "0 auto",
+  display: "grid",
+  gridTemplate: "repeat(3, 1fr) / repeat(3, 1fr)",
 };
 
 export const squareStyles = {
-    background: 'lightblue',
-    border: '5px solid darkblue',
-    fontSize: '50px',
-    fontWeight: '800',
-    cursor: 'pointer',
-    outline: 'none',
-    ':hover': {
-        background: 'skyblue'
-    }
+  background: "lightblue",
+  border: "5px solid darkblue",
+  fontSize: "50px",
+  fontWeight: "800",
+  cursor: "pointer",
+  outline: "none",
+  ":hover": {
+    background: "skyblue",
+  },
+};
+
+export const loginDivStyle = {
+  display: "grid",
+  justifyContent: "center",
+  alignItems: "center",
 };
 
 export const loginButtonStyles = {
-    backgroundColor: 'white',
-    borderStyle: 'none',
-    margin: '0 5px 10px 5px',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'contain',
-    width: '100px',
-    height: '40px',
-    backgroundImage: `url(${require('./assets/google.png')})`,
-    display: 'inline-block',
-    borderRadius: '5px',
-    border: 'thin solid lightgray',
-    whiteSpace: 'nowrap',
-}
+  backgroundColor: "white",
+  borderStyle: "none",
+  margin: "0 5px 10px 5px",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "contain",
+  width: "100px",
+  height: "40px",
+  backgroundImage: `url(${require("./assets/google.png")})`,
+  display: "inline-block",
+  borderRadius: "5px",
+  border: "thin solid lightgray",
+  whiteSpace: "nowrap",
+};
