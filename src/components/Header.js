e3a73a9b0427dd5dcd26a8a069b4d0e460b8755b
Dan Bennett <dghbennett47@gmail.com>
2020-07-14 12:17:47 +0100

    feat: have header change based on log in status

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,23 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../Auth';
 import { headerStyles } from '../styles';
+import googleLogOut from '../helpers';
 
 const Header = () => {
-    const currentUser = useContext(AuthContext);
+    const userContext = useContext(AuthContext);
 
-    if (currentUser) {
-        const nameOfUser = currentUser.currentUser.displayName;
+    if (userContext.currentUser) {
+        const nameOfUser = userContext.currentUser.displayName;
         const firstNameOfUser = nameOfUser.substr(0, nameOfUser.indexOf(' '));
         const welcomeText = `Hi, ${firstNameOfUser}`;
         return (
             <>
                 <div style={headerStyles}>
-                    <h1>{welcomeText}</h1>
+                    <h1>Tic Tac Pro</h1>
+                    <div>{welcomeText}</div>
+                </div>
+                <div>
+                    <button onClick={googleLogOut}>Sign Out</button>
                 </div>
             </>
         );
